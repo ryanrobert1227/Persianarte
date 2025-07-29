3fd170592272bab526f5c5c1a0a6554fdf715e5f
ryanrobert1227 <ryanrobert2504@gmail.com>
2025-07-29 19:40:13 -0300

    fix: change fontsize

--- src/components/Buttons/ButtonsTemplate.tsx
+++ src/components/Buttons/ButtonsTemplate.tsx
@@ -3,7 +3,7 @@ import { Button } from '@chakra-ui/react'
 function ButtonsTemplate(props: any) {
   return (
     <Button border="0.15em solid black" borderRadius="0" 
-    fontFamily='Public Sans' fontStyle="normal" fontWeight="400" fontSize={["1.5rem", "", "", "1rem"]} p={["2rem", "", "", "1.5rem"]}
+    fontFamily='Public Sans' fontStyle="normal" fontWeight="400" fontSize="1rem" p="1.5rem"
     lineHeight="0.7rem" letterSpacing="2px" textTransform="uppercase" color="#33323D" onClick={props.OnClick}>{props.msg}</Button>
   )
 }
